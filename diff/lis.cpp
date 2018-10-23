fac6a1cddca4d5b60e99f032c588f88700f7c4db
Zhifei Yang <zhiyang@ethz.ch>
2018-10-23 17:04:32 +0200

    without binary_search yet

--- diff/lis.cpp
+++ diff/lis.cpp
@@ -21,6 +21,20 @@
 #include <cstring>
 #include <climits>
 #include <vector>
+#include <algorithm>
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+// temporary
+size_t binary_search(const std::vector<int>& M,
+                           size_t low,
+                           size_t high,
+                           int key)
+{
+    size_t position = std::lower_bound(M.begin() + low, M.begin() + high, key) - M.begin();
+    return position;
+}
 
 /** Read input from stdin
  *
@@ -28,30 +42,32 @@
  *  and reserves enough space for M
  */
 void get_input(std::vector<int>& A, 
-               unsigned int& N, 
-               std::vector<unsigned int>& M) 
+               size_t& N, 
+               std::vector<int>& M) 
 {
     int x;
     while (scanf("%d", &x) == 1) 
     {
         A.push_back(x);
     }
-    N = (unsigned int) A.size();
+    N = A.size();
     A.resize(N+1);  // for holding A[N]
+    M.resize(N+1);
 }
 
 int main() 
 {
     std::vector<int> A;  // input sequence
-    unsigned int N;  // length of A
-    unsigned int n;  // loop variable
-    unsigned int m;  // TBA
-    unsigned int l;  // TBA
-    std::vector<unsigned int> M;  // TBA auxiliary array
+    size_t N;  // length of A
+    size_t n;  // loop variable
+    size_t m;  // TBA
+    size_t l;  // TBA
+    std::vector<int> M;  // TBA auxiliary array
     get_input(A, N, M);
-    // for (auto i : A) printf("%d\n", i);
 
-    // Assign A[N] with "something"
+    // Assign A[N] with "something", I choose INT_MAX so that it will not have
+    // a chance to change M in the statement M[l] = min(M[l], A[n + 1]) of the
+    // last round.
     A[N] = INT_MAX;
     n = m = l = 0;
     // Invariant `UpSequence': 
@@ -65,8 +81,8 @@ int main()
     // `UpSequence' is true here, because n == 0, and the length of an empty set
     // is 0.
     // `Prefix' is true here, because the prefix of A[0] is empty.
-    M[0] = 0;  // why 0? why isn't it INT_MAX? counterexample: -100 -98 -99
-    for (int i = 0; i < N; ++i)
+    M[0] = A[0];  // why 0? why isn't it INT_MAX? counterexample: -100 -98 -99
+    for (size_t i = 1; i < N; ++i)
     {
         M[i] = INT_MAX;
     }
@@ -76,5 +92,23 @@ int main()
     // Invariant `MSorted':
     //   M is sorted, i.e. if i < j then M[i] < M[j] (cannot be M[i] == M[j])
 
+    // `MinimalEndpoint' is true here as m == n == 0, and M[0] == A[0].
+    // `MSorted' is true here, obviously by the above assignments.
+    while (n != N) {
+        // `UpSequence', `Prefix', `MinimalEndpoint' 
+        // and `MSorted' are all true here, as nothing as changed.
+        m = max(m, l + 1);
+        // `UpSequence' is true up to n+1 here.
+        l = binary_search(M, 0, m + 1, A[n + 1]);
+        // M[0, l) < A[n+1] <= M[l, m+1), so `Prefix' is true up to n+1 here.
+        M[l] = min(M[l], A[n + 1]);
+        // `MinimalEndpoint' is true up to n+1 here.
+        // `MSorted' is true here.
+        n = n + 1;
+        // `UpSequence', `Prefix', `MinimalEndpoint' 
+        // and `MSorted' are all true up to n.
+    }
+
+    printf("%lu\n", m);
     return 0;
 }
