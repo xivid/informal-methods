13b0a981306ecdfeed93b6139b7abed4b5160c09
Zhifei Yang <zhiyang@student.ethz.ch>
2018-12-05 23:31:22 +0100

    T4 submitted

--- dijkstra/T4.py
+++ dijkstra/T4.py
@@ -1,22 +1,47 @@
 import sys; INFTY= 1000
 
-def heapify(hp, idxLst):
+def heappop(hp, idx):
     '''
-    Arrange `hp` into a heap and store the index of pair (m[i], i) in `idxLst[i]`, in O(N) time.
+    Pop the minimum element and re-establish the heap invariant, in O(logN) time.
+    Restore the heap invariant by first swapping the last element to the first and deleting the last element, then repeatedly moving the first element down until its m-value <= its children's. Meanwhile, `idx` is updated.
     '''
-    pass
+    # update top and remove redundancy
+    hp[0] = hp[-1]; del hp[-1]
+    if not hp:  # hp becomes empty
+        return
+    idx[hp[0][1]] = 0
+    # re-heapify
+    i, n = 0, len(hp)
+    while 2*i+1 < n:  # while node i has at least the left child
+        smallest = i
+        if hp[2*i+1] < hp[i]:
+            smallest = 2*i+1
+        elif 2*i+2 < n and hp[2*i+2] < hp[i]:
+            smallest = 2*i+2
+        if smallest == i:  # heap invariant restored, adjusting finished
+            break
+        # otherwise do the swap and continue testing with new children
+        idx[hp[smallest][1]] = i
+        idx[hp[i][1]] = smallest
+        hp[smallest], hp[i] = hp[i], hp[smallest]
+        i = smallest
 
-def heappop(hp, idxLst):
-    '''
-    Pop the minimum element and re-establish the heap invariant by updating `hp` and `idxLst`, in O(logN) time.
-    '''
-    pass
 
 def heapupdate(hp, idx, (newD, nTo)):
     '''
-    Update the m-value of hp[idx[nTo]] and re-establish the heap invariant by updating `hp` and `idxLst`, in O(logN) time.
+    Decrease the m-value of hp[idx[nTo]] to newD and re-establish the heap invariant, in O(logN) time.
+    Restore the heap invariant by repeatedly moving the updated element upwards (until its m-value >= its parent's), and update `idx` meanwhile.
     '''
-    pass
+    hp[idx[nTo]] = (newD, nTo)
+    # Since it's decreased, it could only be moved upwards but not downwards
+    i = idx[nTo]
+    parent = (i-1)//2
+    while i > 0 and hp[i] < hp[parent]:
+        idx[hp[i][1]] = parent
+        idx[hp[parent][1]] = i
+        hp[i], hp[parent] = hp[parent], hp[i]
+        i = parent
+        parent = (i-1)//2  # becomes negative when i == 0 but doesn't matter because it will never be accessed (we have loop guard i>0)
 
 print "How many nodes? ",
 N= int(sys.stdin.readline()) # N= number of nodes.
@@ -29,10 +54,9 @@ for line in sys.stdin.readlines():
     nFrom, dist, nTo = map(int, line.split())
     G2[nFrom].append((dist, nTo))  # put every arc in the respective list
 m= [INFTY]*N; m[0]= 0   # m:=  initially INFTY except for initial node
-#>> un1 = [1]*N; nun1 = N   # un1[i] == 1 for all nodes: all nodes are unmarked
-hp3 = [(m[i], i) for i in range(N)]  # "(m[i], i) in hp3" iff "un1[i] == 1"; and len(hp3) == nun1
-idx3 = list(range(N))  # idx3[i] is the index of pair (m[i], i) in hp3 if un1[i] == 1, otherwise idx3[i] == N
-heapify(hp3, idx3)
+#>> un1 = [1]*N; nun1 = N
+hp3 = [(m[i], i) for i in range(N)]
+idx3 = list(range(N))
 #<<
 tn= 0                   # tn:= initial node
 
@@ -44,7 +68,7 @@ tn= 0                   # tn:= initial node
 while 1:
 #>> un1[tn] = 0; nun1 -= 1
 #>> if nun1 == 0: break
-    heappop(hp3,idx3)  # mark tn, which is at the heap top
+    heappop(hp3, idx3)  # mark tn, which is at the heap top
     if len(hp3) == 0: break
 #<<
 
@@ -57,7 +81,7 @@ while 1:
             if newD<m[nTo]:
                 m[nTo]= newD
 #>>
-                heapupdate(hp3, idx3, (newD, nTo))  # update hp3[idx3[nTo]] with newD and re-establish the heap invariant (also updates idx3[])
+                heapupdate(hp3, idx3, (newD, nTo))
 #<<
 
     # Re-establish I3. // H.
